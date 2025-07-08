ccf67fed71e44e0042a8f59f0adeeaa966585986
Sana Ullah <your-email@example.com>
2025-07-08 20:52:25 +0300

    Rename shop.js to temp.js to force casing update

--- routes/temp.js
+++ routes/temp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Shop = require('../models/Shop');
+const Shop = require('../models/temp');
 
 // ✅ GET: Fetch shop info
 router.get('/', async (req, res) => {
